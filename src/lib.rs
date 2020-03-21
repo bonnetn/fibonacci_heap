f8b00ef38f91084314e3d6abb49726f53da112e0
Nicolas Bonnet <mail@nicolasbon.net>
2020-03-21 23:43:26 +0100

    Add decrease_key

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-use std::alloc::handle_alloc_error;
 use std::cmp::Ordering;
 use std::collections::HashMap;
 
@@ -8,18 +7,25 @@ use crate::heap::Heap;
 
 mod heap;
 
-struct NodeID(usize);
+struct NodeID(usize, usize);
 
 struct TreeNode<T> {
     parent: Option<usize>,
     element: T,
     marked: bool,
     children: Vec<usize>,
+    handle_id: usize,
 }
 
 impl<T> TreeNode<T> {
-    fn new(element: T, parent: Option<usize>, handle: &NodeID) -> TreeNode<T> {
-        TreeNode { parent, element, marked: false, children: Vec::new()}
+    fn new(element: T, parent: Option<usize>, handle_id: usize) -> TreeNode<T> {
+        TreeNode {
+            parent,
+            element,
+            marked: false,
+            children: Vec::new(),
+            handle_id,
+        }
     }
 
     fn degree(&self) -> usize {
@@ -27,19 +33,21 @@ impl<T> TreeNode<T> {
     }
 }
 
+
 struct FibonacciHeap<T> {
     nodes: Slab<TreeNode<T>>,
     trees: Vec<usize>,
     min_element: usize,
+    id_counter: usize,
 }
 
 impl<T: Ord> heap::Heap<T> for FibonacciHeap<T> {
     type Handle = NodeID;
 
     fn find_minimum(&self) -> Option<&T> {
-        match self.get_node_by_id(self.min_element) {
+        match self.nodes.get(self.min_element) {
             None => None,
-            Some(elem) => Some(&elem.element)
+            Some(elem) => Some(&elem.element),
         }
     }
 
@@ -52,9 +60,9 @@ impl<T: Ord> heap::Heap<T> for FibonacciHeap<T> {
     }
 
     fn insert(&mut self, element: T) -> Self::Handle {
-        let mut handle = NodeID(0);
-        let tree = TreeNode::new(element, None, &handle);
-        NodeID(self.insert_tree(tree))
+        self.id_counter += 1;
+        let tree = TreeNode::new(element, None, self.id_counter);
+        NodeID(self.insert_tree(tree), self.id_counter)
     }
 
     fn extract_minimum(&mut self) -> Option<T> {
@@ -104,13 +112,13 @@ impl<T: Ord> heap::Heap<T> for FibonacciHeap<T> {
         }
         self.trees = degrees_map.values().map(|v| *v).collect();
 
-
         // In the third phase we check each of the remaining roots and find the minimum. This
         // takes O(log n) time and the potential does not change. The overall amortized running
         // time of extract minimum is therefore O(log n).
-        self.min_element = *self.trees
+        self.min_element = *self
+            .trees
             .iter()
-            .map(|tree_id| (tree_id, self.get_node_by_id(*tree_id).unwrap()))
+            .map(|tree_id| (tree_id, self.nodes.get(*tree_id).unwrap()))
             .min_by_key(|(_, tree)| tree.degree())
             .map(|(tree_id, _)| tree_id)
             .unwrap();
@@ -122,35 +130,41 @@ impl<T: Ord> heap::Heap<T> for FibonacciHeap<T> {
         // The following comments were extracted from the Fibonacci heap article on Wikipedia.
         // https://en.wikipedia.org/w/index.php?title=Fibonacci_heap&oldid=944266509
 
-        // Operation decrease key will take the node, decrease the key and if the heap property
-        // becomes violated (the new key is smaller than the key of the parent), the node is cut
-        // from its parent. If the parent is not a root, it is marked.
-        // If it has been marked already, it is cut as well and its parent is marked.
-        // We continue upwards until we reach either the root or an unmarked node.
-        let node = self.get_node_by_id(handle.0);
+        let node_id = handle.0;
+        let node = self.nodes.get(node_id);
         if let None = node {
             return; // Not in the heap.
         }
         let node = node.unwrap();
+        if node.handle_id != handle.1 {
+            return; // Handle refers to a node that was deleted.
+        }
         if new_element.cmp(&node.element) == Ordering::Greater {
-            return // New element is greater than existing one.
+            return; // New element is greater than existing one.
         }
 
         if let None = node.parent {
-            // Root node.
-            if self.is_minimum(node) {
-                self.min_element = handle.0;
+            // Root node, just update the value and the minimum.
+            self.nodes.get_mut(node_id).unwrap().element = new_element;
+            if self.is_minimum(self.nodes.get(node_id).unwrap()) {
+                self.min_element = node_id;
             }
             return;
         }
 
         let parent_id = node.parent.unwrap();
-        let parent = self.get_node_by_id(parent_id).unwrap();
-        if new_element.cmp(&parent.element) != Ordering::Less {
+        let parent = self.nodes.get(parent_id).unwrap();
+        if parent.element.cmp(&new_element) == Ordering::Less {
             // Heap property not violated, nothing to do.
             return;
         }
 
+        // Operation decrease key will take the node, decrease the key and if the heap property
+        // becomes violated (the new key is smaller than the key of the parent), the node is cut
+        // from its parent. If the parent is not a root, it is marked.
+        // If it has been marked already, it is cut as well and its parent is marked.
+        // We continue upwards until we reach either the root or an unmarked node.
+        self.mark_or_cut(node_id);
 
         // Now we set the minimum pointer to the decreased value if it is the new minimum. In the
         // process we create some number, say k, of new trees. Each of these new trees except
@@ -159,7 +173,11 @@ impl<T: Ord> heap::Heap<T> for FibonacciHeap<T> {
         // −(k − 1) + 1 = − k + 2. Combining these 2 changes, the potential changes by
         // 2(−k + 2) + k = −k + 4. The actual time to perform the cutting was O(k), therefore
         // (again with a sufficiently large choice of c) the amortized running time is constant.
-        self.nodes.get_mut(handle.0).unwrap().element = new_element;
+
+        self.nodes.get_mut(node_id).unwrap().element = new_element;
+        if self.is_minimum(self.nodes.get(node_id).unwrap()) {
+            self.min_element = node_id
+        }
     }
 
     fn delete(&mut self, element: T) {
@@ -167,18 +185,14 @@ impl<T: Ord> heap::Heap<T> for FibonacciHeap<T> {
     }
 }
 
-
 impl<T: Ord> FibonacciHeap<T> {
     fn new() -> FibonacciHeap<T> {
         FibonacciHeap {
             nodes: Slab::new(),
             trees: Vec::new(),
             min_element: 0,
+            id_counter: 0,
         }
-    }
-
-    fn get_node_by_id(&self, id: usize) -> Option<&TreeNode<T>> {
-        self.nodes.get(id)
     }
 
     fn is_minimum(&self, tree: &TreeNode<T>) -> bool {
@@ -189,9 +203,11 @@ impl<T: Ord> FibonacciHeap<T> {
     }
 
     fn remove_tree(&mut self, tree_id_to_remove: usize) -> TreeNode<T> {
-        let index_to_remove = self.trees.iter()
+        let index_to_remove = self
+            .trees
+            .iter()
             .enumerate()
-            .filter(|(index, tree_id)| tree_id_to_remove == **tree_id)
+            .filter(|(_, tree_id)| tree_id_to_remove == **tree_id)
             .map(|(i, _)| i)
             .next()
             .unwrap();
@@ -210,8 +226,12 @@ impl<T: Ord> FibonacciHeap<T> {
         tree_id
     }
 
-    fn merge_or_merge_same_degrees_tree(&mut self, tree_to_insert_id: usize, degrees_map: &mut HashMap<usize, usize>) {
-        let tree_to_insert = self.get_node_by_id(tree_to_insert_id).unwrap();
+    fn merge_or_merge_same_degrees_tree(
+        &mut self,
+        tree_to_insert_id: usize,
+        degrees_map: &mut HashMap<usize, usize>,
+    ) {
+        let tree_to_insert = self.nodes.get(tree_to_insert_id).unwrap();
         let degree = tree_to_insert.degree();
 
         if !degrees_map.contains_key(&degree) {
@@ -220,37 +240,61 @@ impl<T: Ord> FibonacciHeap<T> {
         }
 
         let tree_to_merge_id = degrees_map.remove(&degree).unwrap();
-        let tree_to_merge = self.get_node_by_id(tree_to_merge_id).unwrap();
+        let tree_to_merge = self.nodes.get(tree_to_merge_id).unwrap();
 
         if tree_to_merge.element.cmp(&tree_to_insert.element) == Ordering::Less {
-            self.nodes.get_mut(tree_to_merge_id).unwrap().children.push(tree_to_insert_id);
+            self.nodes
+                .get_mut(tree_to_merge_id)
+                .unwrap()
+                .children
+                .push(tree_to_insert_id);
             self.nodes.get_mut(tree_to_insert_id).unwrap().parent = Some(tree_to_merge_id);
             self.merge_or_merge_same_degrees_tree(tree_to_merge_id, degrees_map);
         } else {
-            self.nodes.get_mut(tree_to_insert_id).unwrap().children.push(tree_to_merge_id);
+            self.nodes
+                .get_mut(tree_to_insert_id)
+                .unwrap()
+                .children
+                .push(tree_to_merge_id);
             self.nodes.get_mut(tree_to_merge_id).unwrap().parent = Some(tree_to_insert_id);
             self.merge_or_merge_same_degrees_tree(tree_to_insert_id, degrees_map);
         }
     }
 
+    fn cut(&mut self, node_id: usize, parent_id: usize) {
+        let mut parent = self.nodes.get_mut(parent_id).unwrap();
+        parent.children = parent
+            .children
+            .iter()
+            .filter(|child| **child != node_id)
+            .map(|child| *child)
+            .collect();
+
+        let mut node = self.nodes.get_mut(node_id).unwrap();
+        node.parent = None;
+
+        self.trees.push(node_id);
+    }
     fn mark_or_cut(&mut self, node_id: usize) {
-        let node = self.get_node_by_id(node_id).unwrap();
+        let node = self.nodes.get(node_id).unwrap();
         let parent_id = node.parent;
         if let None = parent_id {
             // Root node.
-            return
+            return;
         }
         let parent_id = parent_id.unwrap();
         let mut parent = self.nodes.get_mut(parent_id).unwrap();
         if parent.marked {
             // Already marked, we need to cut it.
+            self.cut(node_id, parent_id);
+            self.mark_or_cut(parent_id);
         } else {
             // Not marked yet.
             parent.marked = true;
+            self.cut(node_id, parent_id)
         }
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -282,16 +326,14 @@ mod tests {
         assert_eq!(a.extract_minimum(), None);
         assert_eq!(a.extract_minimum(), None);
 
-
         let handle42 = a.insert(42);
         let handle10 = a.insert(10);
         assert_eq!(a.find_minimum(), Some(&10));
 
-        a.decrease_key(&handle42, 1);
-        assert_eq!(a.find_minimum(), Some(&1));
+        a.decrease_key(&handle42, 2);
+        assert_eq!(a.find_minimum(), Some(&2));
 
-        a.decrease_key(&handle42, 11);
-        assert_eq!(a.find_minimum(), Some(&10));
+        a.decrease_key(&handle10, 1);
+        assert_eq!(a.find_minimum(), Some(&1));
     }
 }
-
